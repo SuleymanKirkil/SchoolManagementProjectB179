adf61de68be9adeb69865ba745a3a305bcf80ee7
Ali Gel <ali@gel.com>
2023-12-24 21:20:19 +0300

    3rd

--- src/test/java/school_management/stepdefinitions/api_stepdefinitions/DeanControllerStepDefs.java
+++ src/test/java/school_management/stepdefinitions/api_stepdefinitions/DeanControllerStepDefs.java
@@ -7,6 +7,9 @@ import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import school_management.pojos.DeanControllerPojo.DeanPojo;
 import school_management.pojos.DeanControllerPojo.DeanPostPojo;
+import school_management.pojos.DeanControllerPojo.ObjectPojo;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static junit.framework.TestCase.assertEquals;
@@ -17,6 +20,9 @@ public class DeanControllerStepDefs {
     DeanPostPojo payLoad;
     Response response;
     DeanPojo actualData;
+    public static Integer userId;
+    ObjectPojo objectPojo;
+    DeanPojo expectedData;
     @Given("User is authorized as {string}")
     public void userIsAuthorizedAs(String role) {
         role = role.trim().toLowerCase();
@@ -74,5 +80,65 @@ public class DeanControllerStepDefs {
        assertEquals(payLoad.getSsn(),actualData.getObject().getSsn());
        assertEquals(payLoad.getSurname(),actualData.getObject().getSurname());
        assertEquals(payLoad.getUsername(),actualData.getObject().getUsername());
+    }
+
+    @And("User gets id of the Dean with username {string}")
+    public void userGetsIdOfTheDeanWithUsername(String userName) {
+        spec.pathParams("first", "dean"
+        ,"second","getAll");
+        response= given(spec).when().get("{first}/{second}");
+        List<Integer> userIdList = response.jsonPath().getList("findAll{it.username=='"+userName+"'}.userId");
+        System.out.println(userIdList+ "list ");
+        userId = userIdList.get(0);
+    }
+
+    @And("User sets the Url for Get Dean By id")
+    public void userSetsTheUrlForGetDeanById() {
+        spec.pathParams("first","dean","second","getManagerById","third",userId);
+    }
+
+    @And("sets the expected data for Get Dean By id")
+    public void setsTheExpectedDataForGetDeanById() {
+        objectPojo = new ObjectPojo("1975-05-05","Turkey","FEMALE","Tom","524-456-7395",
+                "523-65-6879","Sawyer",userId,"TomSawyer");
+        expectedData = new DeanPojo("OK","Dean successfully found",objectPojo);
+
+    }
+
+    @When("sends get request and get response")
+    public void sendsGetRequestAndGetResponse() {
+        response= given(spec).when().get("{first}/{second}/{third}");
+    }
+
+    @And("verifies response body for Get Dean By id")
+    public void verifiesResponseBodyForGetDeanById() {
+        DeanPojo actualData = response.as(DeanPojo.class);
+/*
+{
+    "object": {
+        "userId": 3201,
+        "username": "TomSawyer",
+        "name": "Tom",
+        "surname": "Sawyer",
+        "birthDay": "1975-05-05",
+        "ssn": "523-65-6879",
+        "birthPlace": "Turkey",
+        "phoneNumber": "524-456-7395",
+        "gender": "FEMALE"
+    },
+    "message": "Dean Saved",
+    "httpStatus": "CREATED"
+}
+ */
+        assertEquals(objectPojo.getUserId(),actualData.getObject().getUserId());
+        assertEquals(objectPojo.getUsername(),actualData.getObject().getUsername());
+        assertEquals(objectPojo.getName(),actualData.getObject().getName());
+        assertEquals(objectPojo.getBirthDay(),actualData.getObject().getBirthDay());
+        assertEquals(objectPojo.getSsn(),actualData.getObject().getSsn());
+        assertEquals(objectPojo.getBirthPlace(),actualData.getObject().getBirthPlace());
+        assertEquals(objectPojo.getPhoneNumber(),actualData.getObject().getPhoneNumber());
+        assertEquals(objectPojo.getGender(),actualData.getObject().getGender());
+        assertEquals(expectedData.getHttpStatus(),actualData.getHttpStatus());
+        assertEquals(expectedData.getMessage(),actualData.getMessage());
     }
 }
